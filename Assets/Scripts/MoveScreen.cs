c2ddf30d3f80be3258dc10ea2d5f7ec07297907a
Maria Claverias <maria.claverias@tecsup.edu.pe>
2023-10-09 22:13:07 -0500

    menu, player y score listos

---
+++ Assets/Scripts/MoveScreen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScreen : MonoBehaviour
+{
+    [SerializeField]
+    private float range = -35.28f;
+    [SerializeField]
+    private float speed;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        startPos = transform.position;
+        endPos = startPos;
+    }
+
+    private void Update()
+    {
+        MoveScreenUpdate();
+    }
+
+    private void MoveScreenUpdate()
+    {
+        endPos.x -= speed * Time.deltaTime;
+        transform.position = endPos;
+
+        if (endPos.x <= -35.28f)
+        {
+            transform.position = startPos;
+            endPos = transform.position;
+        }
+    }
+}
--- Assets/Scripts/UpdateScore.cs
+++ Assets/Scripts/UpdateScore.cs
@@ -5,11 +5,12 @@ using TMPro;
 
 public class UpdateScore : MonoBehaviour
 {
-    public TMP_Text scoreText;
+    public TMP_Text[] scoreText;
     public Score score;
 
     void Update()
     {
-        scoreText.text = "Score:\n" + score.score.ToString() + "\n\nHighest Score:\n" + score.highestScore.ToString();
+        scoreText[0].text = score.score.ToString();
+        scoreText[1].text = score.highestScore.ToString();
     }
 }
